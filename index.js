6603be53f8872b08eda9624b2d1e1b3398781c1d
dpalmasan <dpalmasan@gmail.com>
2022-04-30 10:23:58 -0700

    Add resolvers for mutation, and query all photos (#1)

--- index.js
+++ index.js
@@ -2,14 +2,51 @@ import { ApolloServer } from "apollo-server"
 
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
   }
 `
 
+var _id = 0
+var photos = []
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+
+  Mutation: {
+
+    // Parent is always the first argument to a resolver
+    postPhoto(parent, args) {
+
+      let newPhoto = {
+        id: _id++,
+        ...args
+      }
+      photos.push(newPhoto);
+      return newPhoto;
+    }
+  },
+
+  Photo: {
+
+    // parent represent current photo object
+    url: parent => `https://yoursite.com/img/${parent.id}.jpg`
   }
 }
 
